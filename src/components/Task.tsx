319d3c148b1cf024ca8337f634c3b2055ee07f61
fabricio <fabriciossza@gmail.com>
2023-05-29 13:10:58 -0300

    feat(Task): adjusting style of component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,9 +4,9 @@ import { Trash } from "@phosphor-icons/react"
 export function Task() {
     return (
         <div className={styles.task_container}>
-            <input type="checkbox" name="" id="" />
-            <p>Lorem ipsum - task atividade -blabla</p>
-            <Trash />
+            <label className={styles.label}><input type="checkbox" className={styles.checkbox}/><span></span></label>
+            <p>Lorem ipsum - task atividade  - activity - teste teste</p>
+            <button className={styles.delete_button}><Trash size={20}/></button>
         </div>
     )
 }
